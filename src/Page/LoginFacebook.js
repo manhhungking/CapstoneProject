f45c86dedb6d085b89f0cefd797b28706e582dcb
manhhungking <trinhmanhhung03@gmail.com>
2023-06-07 21:28:25 +0700

    push facebook login

--- src/Page/LoginFacebook.js
+++ src/Page/LoginFacebook.js
@@ -13,22 +13,22 @@ export function LoginFacebook() {
   const [email, setEmail] = useState("");
   const notify = useNotify();
   console.log("Facebook!!!");
-  window.FB.login(function(response) {
-    if (response.authResponse) {
-      console.log("Welcome!  Fetching your information.... ");
-      // window.FB.api("/me", function(response) {
-      //   console.log("Good to see you, " + response.name + ".");
-      // });
-    } else {
-      console.log("User cancelled login or did not fully authorize.");
-    }
-  });
+  // window.FB.login(function(response) {
+  //   if (response.authResponse) {
+  //     console.log("Welcome!  Fetching your information.... ");
+  //     window.FB.api("/me", function(response) {
+  //       console.log("Good to see you, " + response.name + ".");
+  //     });
+  //   } else {
+  //     console.log("User cancelled login or did not fully authorize.");
+  //   }
+  // });
 
   return (
     <LoginSocialFacebook
       appId="6284345351642669"
       onResolve={(response) => {
-        console.log(response);
+        console.log("Response: ", response);
       }}
       onReject={(error) => {
         console.log(error);
